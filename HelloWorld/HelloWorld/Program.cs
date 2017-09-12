114047eb6adaa39de6265ff19d461171be9981e7
albert018 <albert2.chen@gmail.com>
2017-09-12 16:57:58 +0800

    test
    2017-09-12 first try

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //20170912
+            Console.WriteLine("test");
+            Console.ReadLine();
+
+        }
+    }
+}
